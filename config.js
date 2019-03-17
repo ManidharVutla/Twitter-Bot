18b0322e8def447afcdbee36622ce3cb3286d134
Manidhar Vutla <vutlamanidhar@gmail.com>
2019-03-17 18:02:51 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    consumer_key:         '9tY3SVaqRmAC14R4LKU8zlM1Q',
-    consumer_secret:      'pGpTmmDPqDjSjIgyl1rFl5N782UOJjKxqt2DUsNKmvR3wxWnFq',
-    access_token:         '839123857567023104-6puRiO3q1Dmz7fknDEKA8jKKkeuXa1u',
-    access_token_secret:  'svojFc9phzxBL4KNQbQr0C0G1op7o3t4krKYff39HKBFv',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
-}
